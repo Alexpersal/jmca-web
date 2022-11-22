dee2aac06225442cfd14c033d41ace704350055e
Alexpersal <97981869+Alexpersal@users.noreply.github.com>
2022-11-22 17:56:42 +0100

    fix scrollTo

--- src/pages/CasasEco.js
+++ src/pages/CasasEco.js
@@ -1,6 +1,10 @@
 import "./CasasEco.css";
+import { useEffect } from "react";
 
 export default function CasasEco() {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, []);
   return (
     <>
       <div class="imgSuperior">
--- src/pages/Reformas.js
+++ src/pages/Reformas.js
@@ -1,6 +1,10 @@
 import "./Reformas.css";
+import { useEffect } from "react";
 
 export default function Reformas() {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, []);
   return (
     <>
       <div class="imgSuperior">
